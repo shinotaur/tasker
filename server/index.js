3eaca27fcd3b67410cdc9f028c7f94abb0e23ec9
shinotaur <71105630+shinotaur@users.noreply.github.com>
2024-09-11 19:20:31 +0530

    added new origin

--- server/index.js
+++ server/index.js
@@ -1,19 +1,18 @@
-import cookieParser from "cookie-parser";
-import express from "express";
-import dotenv from "dotenv";
-import morgan from "morgan";
-import cors from "cors";
-import {routeNotFound, errorHandler} from "./middlewares/errorMiddleware.js";
-import {dbConnection} from "./utils/index.js";
-import routes from "./routes/index.js";
-
+import cookieParser from 'cookie-parser';
+import express from 'express';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+import cors from 'cors';
+import { routeNotFound, errorHandler } from './middlewares/errorMiddleware.js';
+import { dbConnection } from './utils/index.js';
+import routes from './routes/index.js';
 
 dotenv.config();
-dbConnection()
+dbConnection();
 
 const app = express();
 
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -21,7 +20,7 @@ app.use(express.json());
 
 app.use(cookieParser());
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 const port = process.env.PORT;
 
@@ -29,14 +28,19 @@ app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
 
-app.use(cors({
-  origin: ["http://localhost:3000", "https://localhost:3001"],
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true,}))
+app.use(
+  cors({
+    origin: [
+      'http://localhost:3000',
+      'https://localhost:3001',
+      'https://tasker-shrey.netlify.app/',
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+  })
+);
 
-app.use("/api", routes );
+app.use('/api', routes);
 
 app.use(routeNotFound);
 app.use(errorHandler);
-
-

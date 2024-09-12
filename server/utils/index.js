b2394374ad5c5905ea8e4be4c2f0bfbc0659e40b
shinotaur <71105630+shinotaur@users.noreply.github.com>
2024-09-12 17:53:18 +0530

    updated utils/index

--- server/utils/index.js
+++ server/utils/index.js
@@ -1,24 +1,24 @@
-import mongoose from "mongoose";
-import jwt from "jsonwebtoken";
+import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken';
 
 export const dbConnection = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
 
-    console.log("DB connection established");
+    console.log('DB connection established');
   } catch (error) {
-    console.log("DB Error: " + error);
+    console.log('DB Error: ' + error);
   }
 };
 
 export const createJWT = (res, userId) => {
-  const token = jwt.sign({userId}, process.env.JWT_SECRET, {
-    expiresIn: "30d",
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
   });
-  res.cookie("token", token, {
+  res.cookie('token', token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV !== "development",
-    sameSite: "strict", //prevent CSRF attack
+    secure: process.env.NODE_ENV !== 'development',
+    sameSite: 'none', //prevent CSRF attack
     maxAge: 30 * 24 * 60 * 60 * 1000, //30 days
   });
 };
